345b264b561e50b574244a94da071e46d1f7ab25
BankShotZombies <60952462+BankShotZombies@users.noreply.github.com>
2020-04-03 15:29:00 -0700

    Project

---
+++ dcp18/dcp18.py
@@ -0,0 +1,19 @@
+
+def maxOfSubarrays (k, arr):
+    subArrays = getSubArrays(k, arr)
+    maxArr = []
+    for arr in subArrays:
+        maxArr.append(max(arr))
+    return maxArr
+
+def getSubArrays (k, arr):
+    i = 0
+    subArrayAdd = 0
+    subarrays = []
+    for i in range (0, len(arr)):
+        if subArrayAdd <= len(arr) - k:
+            subarrays.append(arr[0 + subArrayAdd:k + subArrayAdd])
+            subArrayAdd += 1
+    return subarrays
+
+print(maxOfSubarrays(4, [2, 3, 5, 6, 8, 10, 12, 13, 14, 15]))
